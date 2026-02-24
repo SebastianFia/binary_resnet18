323ad5e0c52b3451f987e506faa447a6f1719efa
seba42 <fiasebastian.04@gmail.com>
2026-02-24 16:50:43 +0100

    single run: finetune + binarize

--- src/config.py
+++ src/config.py
@@ -3,14 +3,17 @@ from typing import List, Any
 
 LR = 1e-3
 LR_MIN = 1e-5
+NOBIN_FRAC = 0.15 # Fraction of training to stay fully float
+FASTBIN_FRAC = 0.30 # We binarize more aggressively until this point
+FASTBIN_END_RATIO = 0.5
 
 @dataclass
 class Config:
     batch_size: int = 128
     weight_decay: float = 1e-4
     
-    epochs_finetune: int = 50
-    epochs_binarize: int = 150
+    # epochs_finetune: int = 50
+    epochs_binarize: int = 200
     
     d_out: int = 100
     p_min: float = 0.0
@@ -35,17 +38,17 @@ class Config:
         [1.0, "cosine", LR, LR_MIN]
     ])
     schedule_phases_bin_ratio: list = field(default_factory=lambda: [
-        [1.0, "circle", 0.0, 1.0]
-        # [0.3, "cosine", 0.0, 0.5],
-        # [1.0, "cosine", 0.5, 1.0]
+        [NOBIN_FRAC, "stay", 0.0, 0.0],       
+        [FASTBIN_FRAC, "cosine", 0.0, FASTBIN_END_RATIO],
+        [1.0, "cosine", FASTBIN_END_RATIO, 1.0], 
+    ])
+    schedule_phases_p: list = field(default_factory=lambda: [
+        [NOBIN_FRAC, "stay", 0.0, 0.0],       
+        [FASTBIN_FRAC, "cosine", 0.0, FASTBIN_END_RATIO],
+        [1.0, "cosine", FASTBIN_END_RATIO, 1.0], 
     ])
     schedule_phases_bin_loss_lambda: list = field(default_factory=lambda: [
         [1.0, "circle", 0.0, 1.0]
-    ])
-    schedule_phases_p: list = field(default_factory=lambda: [
-        [1.0, "circle", 0.0, 1.0]
-        # [0.3, "cosine", 0.0, 0.5],
-        # [1.0, "cosine", 0.5, 1.0]
     ])
     schedule_phases_mixup_cutmix_prob: list = field(default_factory=lambda: [
         [1.0, "stay", 1.0, 1.0]
